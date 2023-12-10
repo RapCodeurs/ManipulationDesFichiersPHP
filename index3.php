8978e0f01e2dc1127f1b1ba03b98e1b42a069013
RapCodeurs <maureljoumessi9@gmail.com>
2023-12-10 20:06:21 +0100

    Nouveaux changements

--- index3.php
+++ index3.php
@@ -1,2 +1,24 @@
+<?php
+	if ($_SERVER['REQUEST_METHOD'] === "POST") {
+	    $file = $_FILES['file']['name'];
+	    $destination = "uploads/";
+	    $target_dir = $destination . basename($file);
 
+	if (file_exists($target_dir)) {
+	    $backup_file = $destination . 'backup' . basename($file); 
+	    copy($target_dir, $backup_file);
+	    $rename_file = $destination . 'rename' . basename($file);
+	    rename($target_dir, $rename_file);
+	    unlink($backup_file);
+	    echo "Le fichier a bien été remplacé avec succès";
 
+	} else { 
+        move_uploaded_file($_FILES['file']['tmp_name'], $target_dir);
+        echo "Le fichier ".$file." a bien été téléchargé";
+    } 
+	
+	}else { 
+	    echo "Il y a eu une erreur lors du téléchargement du fichier";
+	}
+
+	
